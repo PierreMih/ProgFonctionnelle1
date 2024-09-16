93a8d351972b7ac93c614ba29db31db723521770
PMihMAN <pierre.mih@manuloc.eu>
2024-09-16 15:25:24 +0200

    exo 1 remove + exo 2

--- ProgFonctionnelle1/Program.fs
+++ ProgFonctionnelle1/Program.fs
@@ -1,4 +1,6 @@
-﻿type Fruit =
+﻿open System.Linq
+
+type Fruit =
     | Apple = 1
     | Pear = 2
     | Pineapple = 3
@@ -21,21 +23,33 @@ let pineappleCount (fruits: Fruit list) =
 
 let displayCountForEachFruit (fruits: Fruit list) =
     let listOfPossibleFruits = Fruit.GetValues()
+    printfn ""
     for possibleFruit in listOfPossibleFruits do
         let thisFruitCount = fruitCount fruits possibleFruit
         let addSIfRequired (count: int) =
             if count > 1 then "s" else ""
         printfn $"{thisFruitCount} {Fruit.GetName(possibleFruit)}{addSIfRequired thisFruitCount}"
+    printfn ""
 
 displayCountForEachFruit fruitsListComplete
-printfn ""
 
 let rec addNfruitsToList (fruits : Fruit list) (fruitToAdd : Fruit) (n : int) =
-    let n2 = n-1
     if(n > 0) then
         let fruitsResult = fruitToAdd :: fruits
+        let n2 = n-1
         addNfruitsToList fruitsResult fruitToAdd n2 
     else
         fruits
 let fruitsListComplete2 = addNfruitsToList fruitsListComplete Fruit.Pineapple 100
 displayCountForEachFruit fruitsListComplete2
+
+let RemoveOneFruitFromList (fruits : Fruit list) (fruitToRemove : Fruit) =
+    let isFruitToRemove fruit = fruit = fruitToRemove
+    let indexToRemove = fruits |> List.findIndex isFruitToRemove
+    List.removeAt indexToRemove fruits
+    // fruits
+    //     |> List.findIndex isFruitToRemove fruits
+    //     |> List.removeAt
+
+let fruitsListComplete3 = RemoveOneFruitFromList fruitsListComplete2 Fruit.Pineapple
+displayCountForEachFruit fruitsListComplete3
---
+++ ProgFonctionnelle1/exo2.ts
@@ -0,0 +1,63 @@
+﻿// Crée un OBJET Typescript
+let fruitsStock = [
+    { id: 1, name: "Pomme", quantity: 10 },
+    { id: 2, name: "Poire", quantity: 5 },
+    { id: 3, name: "Ananas", quantity: 8 }
+];
+
+// Ne retourne pas de résultat
+function addFruitToStock(name: string, quantity: number) {
+    // utilisation de fruitsStock, externe à la fonction
+    // p.name est une propriété (objet)
+    // .find peut être acceptable
+    // @ts-ignore
+    const existingProduct = fruitsStock.find((p) => p.name === name);
+
+    if (existingProduct) {
+        // modifie une propriété d'instance d'objet
+        existingProduct.quantity += quantity;
+    } else {
+        fruitsStock.push({ id: fruitsStock.length + 1, name, quantity });
+    }
+}
+
+function deleteFruit(name: string) {
+    fruitsStock = fruitsStock.filter((p) => p.name !== name);
+    console.log(`${name} deleted from stock`);
+}
+
+function showStock() {
+    fruitsStock.forEach((fruit) => {
+        console.log(`Fruit : ${fruit.name} | Quantity : ${fruit.quantity}`);
+    });
+}
+
+function sellFruit(name: string, quantity: number) {
+    const fruit = fruitsStock.find((p) => p.name === name);
+
+    if (fruit && fruit.quantity >= quantity) {
+        fruit.quantity -= quantity;
+        console.log(`${quantity} ${name} sold`);
+    } else {
+        console.log(`Not enough ${name} or unknown fruit`);
+    }
+}
+
+addFruitToStock("Pomme", 5);
+addFruitToStock("Citron", 10);
+sellFruit("Ananas", 2);
+showStock();
+deleteFruit("Ananas");
+showStock();
+
+/*
+2 Ananas sold
+Fruit : Pomme | Quantity : 15
+Fruit : Poire | Quantity : 5
+Fruit : Ananas | Quantity : 6
+Fruit : Citron | Quantity : 10
+Ananas deleted from stock
+Fruit : Pomme | Quantity : 15
+Fruit : Poire | Quantity : 5
+Fruit : Citron | Quantity : 10
+*/
